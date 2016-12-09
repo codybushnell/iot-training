9018cff432ae9c877c37bddbf6388686a7aafc5d
Cody Bushnell <cody.bushnell@controlsdata.com>
2016-12-09 00:24:58 -0600

    clean up sensor http api

--- dht-api.py
+++ dht-api.py
@@ -1,22 +1,15 @@
 #!/usr/bin/python3
-from flask import Flask, Response, jsonify, request
-import json
+from flask import Flask, jsonify
 import Adafruit_DHT
-import numpy as np
 
 app = Flask(__name__)
 
 @app.route('/sensor-measurements')
 def sensor_measurements():
-    readings = []
-    for i in range(int(request.args.get('samples'))):
-        measurements = Adafruit_DHT.read_retry(11, 4)
-        if (measurements[0] is not None) & (measurements[1] is not None):
-            readings.append(measurements)
     
-    humidity, temp_C = np.array(readings).T.mean(axis=1).tolist()
+    humidity, temp_C = Adafruit_DHT.read_retry(11, 4)
     temp_F = ((9/5)*temp_C) + 32
     return(jsonify(dict(humidity=humidity, temp_C=temp_C, temp_F=temp_F)))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5555)
+    app.run(host='0.0.0.0')
