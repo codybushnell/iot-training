9575c023d4ef0a1339f1e51cc62963470fe388e6
Cody Bushnell <cody.bushnell@controlsdata.com>
2016-12-09 02:35:00 -0600

    cleaned up pubnub script

--- pubnub_dht.py
+++ pubnub_dht.py
@@ -1,20 +1,13 @@
 #!/usr/bin/python3
 import Adafruit_DHT
-import numpy as np
 from pubnub import Pubnub
 import time
 
 pubnub = Pubnub(publish_key='demo', subscribe_key='demo')
 
-for i in range(20):
+while True:
 
-    readings = []
-    for i in range(2):
-        measurements = Adafruit_DHT.read_retry(11, 4)
-        if (measurements[0] is not None) & (measurements[1] is not None):
-            readings.append(measurements)
-
-    humidity, temp_C = np.array(readings).T.mean(axis=1).tolist()
+    humidity, temp_C = Adafruit_DHT.read_retry(11, 4)
     temp_F = ((9/5)*temp_C) + 32
 
     pubnub.publish('temperature', {'eon': {'temperature_f': temp_F, 'temperature_c': temp_C}})
